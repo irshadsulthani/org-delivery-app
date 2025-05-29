d2b05431662cc2c9f388be11ec378cf9ece0c3c3
irshadsulthani <irshadpallar2020@gmail.com>
2025-05-29 21:22:00 +0530

    changes

--- server/src/interfaces/controllers/UserController.ts
+++ server/src/interfaces/controllers/UserController.ts
@@ -43,6 +43,9 @@ export class UserController {
       res.status(statusCode).json({ message });
     }
   };
+
+
+  
   static  getProfile = async(req: Request, res: Response): Promise<void>=> {
     try {
       console.log("its getting here in getProfile");
